07a0a2ed649fb94e6b7520c3ba84fd3bf50e827e
ShanaFlash <nancy17flash@gmail.com>
2024-06-15 22:22:21 -0600

    fix landing page responsive design

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,14 +1,14 @@
-import './Banner.css'
+import './Footer.css'
 import createxIcon from '../../assets/sd.png'
 import gtIcon from '../../assets/gt3.png'
-const Banner = () => {
+const Footer = () => {
 
     return(
-        <div className="banner">
+        <div className="footer">
             <h3>Launched through Create-X Startup Launch 2024</h3>
             <img className='createx' src={createxIcon} alt="" />
             <img className='gt' src={gtIcon} alt="" />
         </div>
     )
 }
-export default Banner
+export default Footer
